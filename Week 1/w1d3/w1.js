eb3aa636ec6e170ec50203250a5632ea0f43500c
aldiansyahh <aldiansyahaldi861@example.com>
2024-04-24 23:19:19 +0700

    Week 1 day 3

---
+++ Week 1/w1d3/w1.js
@@ -0,0 +1,19 @@
+alert("Hello World");
+const category = document.getElementById('category');
+category.style.color = 'pink';
+category.innerHTML = 'Mantap Kategori';
+
+// import React, { Component } from 
+// 'react';
+
+// class App extends Component {
+//     render() {
+//         return ( 
+//             <div>
+//             <h1> Hello World </h1> 
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
